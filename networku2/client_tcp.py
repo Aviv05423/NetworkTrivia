2a2c0c82622a9962031fd6e57628dd4a79695403
avivv <>
2021-05-21 20:26:02 +0300

    V3  -4.1

--- networku2/client_tcp.py
+++ networku2/client_tcp.py
@@ -2,7 +2,22 @@ import socket
 
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # my_socket.connect((IP, PORT))
-my_socket.connect(("127.0.0.1", 8822))
+# my_socket.connect(("127.0.0.1", 8822))
+#my_socket.connect(("172.31.6.11", 22))
+# my_socket.connect(("ec2-18-222-216-47.us-east-2.compute.amazonaws.com", 22))
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(("172.31.6.11", 22))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
 
 data = ""
 while data != "Bye":
--- networku2/server_tcp.py
+++ networku2/server_tcp.py
@@ -1,4 +1,13 @@
+
+
 import socket
+## getting the hostname by socket.gethostname() method
+hostname = socket.gethostname()
+## getting the IP address using socket.gethostbyname() method
+ip_address = socket.gethostbyname(hostname)
+## printing the hostname and ip_address
+print(f"Hostname: {hostname}")
+print(f"IP Address: {ip_address}")
 
 from datetime import date
 today = date.today()
