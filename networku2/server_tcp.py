808925649dfe6f7b127b085521bf9850c6cc62a0
avivv <>
2021-05-21 23:04:12 +0300

    V3  -4.1

--- networku2/server_tcp.py
+++ networku2/server_tcp.py
@@ -15,7 +15,7 @@ today = date.today()
 import random
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("5.28.133.25", 22))
+server_socket.bind(("0.0.0.0", 22))
 server_socket.listen()
 print("server is run!")
 
