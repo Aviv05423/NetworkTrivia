fa9236c1387b3b3884fb712d5fb4dbbe93c4b154
avivv <>
2021-05-22 18:45:18 +0300

    V4  -discord

--- TriviaDiscord/bot.py
+++ TriviaDiscord/bot.py
@@ -7,4 +7,4 @@ client = commands.Bot(command_prefix=".")
 async def on_ready():
     print("im ready")
 
-client.run("ODQ1Njg1ODM2NDk0NDA1NjU0.YKkkTw.RYlFkiiqSvrH9wr0_R0YRabP6G0")
+client.run(input("toket: "))
