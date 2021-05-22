782657d169cdcfa7efae1feceae6a12f80962340
avivv <>
2021-05-22 22:35:15 +0300

    V4  -telegram

--- TriviaDiscord/bot.py
+++ TriviaDiscord/bot.py
@@ -1,10 +1,17 @@
-import discord
-from discord.ext import commands
+import telebot
 
-client = commands.Bot(command_prefix=".")
 
-@client.event
-async def on_ready():
-    print("im ready")
+API_KEY = "1893608043:AAGYZMlDfPfCOIAvZuc3_XLK9D7VDag38eM"
 
-client.run(input("toket: "))
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['login'])
+def login(message):
+    bot.reply_to(message, "hey")
+
+@bot.message_handler(commands=['hi'])
+def hi(message):
+    bot.send_message(message.chat.id, "hey")
+
+
+bot.polling()
